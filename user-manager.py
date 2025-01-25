6ccf7b1d045ce07440369a3f1b907a3a453d5987
Kacper Zieliński <zielinski_kacper@outlook.com>
2025-01-25 15:09:06 +0100

    feat: Add main loop.

--- user-manager.py
+++ user-manager.py
@@ -124,8 +124,12 @@ class UserManager:
                 return []
             else:
                 with open(USERS_FILE, 'r') as file:
-                    users = json.load(file)
-                    return users
+                    try:
+                        users = json.load(file)
+                        return users
+                    except json.JSONDecodeError:
+
+                        logger.error("Nie udało się załadować danych użytkowników. Plik z użytkownikami jest uszkodzony.")
     
     def add_user(user: dict):
         """Add new user to file.
@@ -137,12 +141,18 @@ class UserManager:
             dict containing user's data
         """
         if not UserManager.validate_email(user["email"]):
-            print("Adres e-mail użytkownika jest nieprawidłowy.")
+            print("Adres e-mail użytkownika jest nieprawidłowy.\n")
             logger.error(f"Nie udało się dodać użytkownika o nazwie \"{user["username"]}\". Adres e-mail jest nieprawidłowy.")
+            return
         users = UserManager.load_users()
+        # if not users:
+            # print("Wystąpił błąd. Plik z użytkownikami jest uszkodzony.\n")
+            # logger.error("Plik z użytkownikami jest uszkodzony.")
+            # return
         with open(USERS_FILE, 'w') as file:
             users.append(user)
             json.dump(users, file, indent=4)
+            print(f"Pomyślnie dodano nowego użytkownika o nazwie \"{user["username"]}\".\n")
             logger.info(f"Dodano nowego użytkownika o nazwie \"{user["username"]}\".")
 
     def remove_user(username: str):
@@ -156,14 +166,18 @@ class UserManager:
 
         """
         users = UserManager.load_users()
+        # if not users:
+            # print("Wystąpił błąd. Plik z użytkownikami jest uszkodzony.\n")
+            # logger.error("Plik z użytkownikami jest uszkodzony.")
+            # return
         updated_users = [u for u in users if u["username"] != username]
         if len(updated_users) != len(users):
             with open(USERS_FILE, 'w') as file:
                 json.dump(updated_users, file, indent=4)
-                print(f"Pomyślnie usunięto użytkownika o nazwie \"{username}\"")
+                print(f"Pomyślnie usunięto użytkownika o nazwie \"{username}\"\n")
                 logger.info(f"Usunięto użytkownika o nazwie \"{username}\".")
         else:
-            print(f"Nie znaleziono użytkownika o nazwie \"{username}\".")
+            print(f"Nie znaleziono użytkownika o nazwie \"{username}\".\n")
             logger.error(f"Nie udało się usunąć użytkownika \"{username}\". Nie znaleziono w pliku użytkownika o takiej nazwie.")
             return
 
@@ -171,11 +185,44 @@ class UserManager:
         """Display all the users from file."""
         users = UserManager.load_users()
         if not users:
-            print("Nie znaleziono żadnych użytkowników w pliku.")
+            print("Nie znaleziono żadnych użytkowników w pliku.\n")
             logger.error("Nie udało się wyświetlić użytkowników. Nie znaleziono żadnych użytkowników w pliku.")
             return
         else:
             for u in users:
                 print(f"Nazwa użytkownika: \"{u["username"]}\" \t Adres e-mail: \"{u["email"]}\"\tRola: \"{u["role"]}\"")
                 logger.info(f"Wyświetlono dane użytkownika \"{u["username"]}\".")
+            print('\n')
             logger.info("Zakończono wyświetlanie danych użytkowników.")
+
+def main():
+    """Main loop."""
+    logger.info("Program został uruchomiony.")
+    while True:
+        print("1 - Wyświetl użytkowników\n2 - Dodaj użytkownika\n3 - Usuń użytkownika\n4 - Wyjdź\n")
+        try:
+            choice = int(input("Wybierz opcję: "))
+        except ValueError:
+            print("Nieprawidłowy wybór.\n")
+            continue
+        if int(choice) not in {1, 2, 3, 4}:
+            print("Nieprawidłowy wybór.\n")
+            continue
+        match choice:
+            case 1:
+                UserManager.display_users()
+            case 2:
+                username = input("Podaj nazwę użytkownika: ")
+                email = input("Podaj adres e-mail: ")
+                role = input("Podaj rolę: ")
+                user = User(username, email, role)
+                UserManager.add_user(user.get_data())
+            case 3:
+                username = input("Podaj nazwę użytkownika do usunięcia: ")
+                UserManager.remove_user(username)
+            case 4:
+                logger.info("Zakończono działanie programu.")
+                break
+
+if __name__ == "__main__":
+    main()
