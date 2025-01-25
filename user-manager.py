f5155c3a54d287bc825f1085fd14ab7060823399
Kacper Zieliński <zielinski_kacper@outlook.com>
2025-01-25 13:21:48 +0100

    feat: Implement logging.

--- user-manager.py
+++ user-manager.py
@@ -5,6 +5,22 @@ import re
 
 USERS_FILE = "users.json"
 
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create handler
+handler = logging.FileHandler("logs.log", encoding="utf-8", mode='w')
+
+# Create formatter
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+# Set formatter for handler
+handler.setFormatter(formatter)
+
+# Add handler to logger
+logger.addHandler(handler)
+
 class User:
     """
     A class used to represent a User.
@@ -104,6 +120,7 @@ class UserManager:
         with open(USERS_FILE, 'w') as file:
             users.append(user)
             json.dump(users, file, indent=4)
+            logger.info(f"Dodano nowego użytkownika o nazwie \"{user["username"]}\".")
 
     def remove_user(username: str):
         """Remove user from file.
@@ -119,8 +136,12 @@ class UserManager:
         updated_users = [u for u in users if u["username"] != username]
         if len(updated_users) != len(users):
             with open(USERS_FILE, 'w') as file:
-                json.dump(updated_users, file)
+                json.dump(updated_users, file, indent=4)
+                print(f"Pomyślnie usunięto użytkownika o nazwie \"{username}\"")
+                logger.info(f"Usunięto użytkownika o nazwie \"{username}\".")
         else:
+            print(f"Nie znaleziono użytkownika o nazwie \"{username}\".")
+            logger.error(f"Nie udało się usunąć użytkownika \"{username}\". Nie znaleziono w pliku użytkownika o takiej nazwie.")
             return
 
     def display_users():
@@ -128,6 +149,27 @@ class UserManager:
         users = UserManager.load_users()
         if not users:
             print("Nie znaleziono żadnych użytkowników w pliku.")
+            logger.error("Nie udało się wyświetlić użytkowników. Nie znaleziono żadnych użytkowników w pliku.")
+            return
         else:
             for u in users:
-                print(f"Nazwa użytkownika: {u["username"]} \t Adres e-mail: {u["email"]} \t Rola: {u["role"]}")
+                print(f"Nazwa użytkownika: \"{u["username"]}\" \t Adres e-mail: \"{u["email"]}\"\tRola: \"{u["role"]}\"")
+                logger.info(f"Wyświetlono dane użytkownika \"{u["username"]}\".")
+            logger.info("Zakończono wyświetlanie danych użytkowników.")
+
+# user1 = User("abcd", "abcd@gmail.com", "role1")
+# UserManager.add_user(user1.get_data())
+# user2 = User("efgh", "efgh@gmail.com", "role2")
+# UserManager.add_user(user2.get_data())
+# user3 = User("1234", "1234@gmail.com", "role3")
+# UserManager.add_user(user3.get_data())
+# user4 = User("xyzv", "xyzv@gmail.com", "role4")
+# UserManager.add_user(user4.get_data())
+# 
+# 
+# 
+# 
+# 
+# UserManager.display_users()
+
+# UserManager.remove_user("1234")
